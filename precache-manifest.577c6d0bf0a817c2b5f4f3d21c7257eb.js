12da6fe3696cd385d58124c8f024fd1552bdf3bf
Roxolana <roxolanapavlyk@gmail.com>
2019-03-29 13:42:16 +0100

    Updates

---
+++ precache-manifest.577c6d0bf0a817c2b5f4f3d21c7257eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "53399bf1a3355d50e3329ab61731c9d6",
+    "url": "/myPortfolio/static/media/video.53399bf1.mp4"
+  },
+  {
+    "revision": "0f24e44b4aafd7f59613",
+    "url": "/myPortfolio/static/css/main.edb69e98.chunk.css"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "9e9ddf919e2337232b46",
+    "url": "/myPortfolio/static/js/2.b03a1b5f.chunk.js"
+  },
+  {
+    "revision": "01c3daa30267ed06d02d1c1a65a3cbdc",
+    "url": "/myPortfolio/static/media/image1.01c3daa3.png"
+  },
+  {
+    "revision": "0f24e44b4aafd7f59613",
+    "url": "/myPortfolio/static/js/main.b263449a.chunk.js"
+  },
+  {
+    "revision": "f6895a962be034e25c515c8400444488",
+    "url": "/myPortfolio/static/media/image2.f6895a96.png"
+  },
+  {
+    "revision": "7450b627e21e26149ba956afea4729fc",
+    "url": "/myPortfolio/static/media/image3.7450b627.png"
+  },
+  {
+    "revision": "f2d9255add0a6ac915c64d0e291e4bdc",
+    "url": "/myPortfolio/static/media/im5.f2d9255a.png"
+  },
+  {
+    "revision": "bff31293d79da91d0ff38a3fa1b917cb",
+    "url": "/myPortfolio/static/media/image4.bff31293.png"
+  },
+  {
+    "revision": "de0d0b7f93990b154cc53a4b00d394c6",
+    "url": "/myPortfolio/index.html"
+  }
+];
